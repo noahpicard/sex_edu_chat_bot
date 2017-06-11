2ea473e3dd4fbf198e31252ff3a5de21e8e09acf
Joan Hong <joanhong@usc.edu>
2017-06-11 01:19:53 -0700

    Merged w jenny

--- app.js
+++ app.js
@@ -11,102 +11,216 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/messenger', function (req, res) {
-	if (req.query['hub.verify_token'] === verify_token) {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
+    if (req.query['hub.verify_token'] === verify_token) {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
 });
 
 app.post('/messenger', function (req, res) {
-  var messaging_events = req.body.entry[0].messaging;
-  for (var i = 0; i < messaging_events.length; i++) {
-    var event = messaging_events[i];
-    if (event.message && event.message.text) {
-      messenger_receive(event);
+    messenger_setgreetingtext();
+    messenger_getstartedbutton();
+    messenger_persistentmenu();
+    var messaging_events = req.body.entry[0].messaging;
+    for (var i = 0; i < messaging_events.length; i++) {
+        var event = messaging_events[i];
+        if (event.message && event.message.text) {
+            messenger_receive(event);
+        }
     }
-  }
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
+var messenger_setgreetingtext = function() {
+    var text = "Let's chat about sexual health!\n";
+    var options = {
+        url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token=' + access_token,
+        method: 'POST',
+        json: {
+            'greeting':[
+                {
+                    'locale':'default',
+                    'text':text
+                }
+            ]
+        }
+    }
+    request(options)
+}
+
+var messenger_persistentmenu = function() {
+    var options = {
+        url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token=' + access_token,
+        method: 'POST',
+        json: {
+            'persistent_menu':[
+                {
+                    'locale':'default',
+                    'composer_input_disabled':true,
+                    'call_to_actions':[
+                        {
+                            'title':'Topics',
+                            'type':'nested',
+                            'call_to_actions':[
+                                {
+                                    'title':'Menstruation',
+                                    'type':'postback',
+                                    'payload':'PAYLOAD'
+                                },
+                                {
+                                    'title':'Sex',
+                                    'type':'postback',
+                                    'payload':'LOCAL_RESOURCES_PAYLOAD'
+                                },
+                                {
+                                    'title':'Contraceptives',
+                                    'type':'postback',
+                                    'payload':'LOCAL_RESOURCES_PAYLOAD'
+                                },
+                                {
+                                    'title':'STDs',
+                                    'type':'postback',
+                                    'payload':'PAYLOAD'
+                                },
+                                {
+                                    'title':'Pregnancy',
+                                    'type':'postback',
+                                    'payload':'PAYLOAD'
+                                }
+                            ]
+                        },
+                        {
+                            'title':'Local Resources',
+                            'type':'postback',
+                            'payload':'LOCAL_REOURCES_PAYLOAD'
+                        }
+                    ]
+                },
+                {
+                    'locale':'zh_CN',
+                    'composer_input_disabled':false
+                }
+            ]
+        }
+    }
+    request(options)
+}
+
+var messenger_getstartedbutton = function () {
+    var options = {
+        url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token=' + access_token,
+        method: 'POST',
+        json: {
+            'get_started':{
+                'payload':'GET_STARTED_PAYLOAD'
+            }
+        }
+    }
+    request(options)
+}
+
 var messenger_receive = function (event) {
-  console.log(event);
-  addMonkeyIfPossible(event);
-  messenger_send(event.sender.id, response.respond(event.sender.id, event.message.text));
+    console.log(event);
+    addMonkeyIfPossible(event);
+    if (event.message.text.includes("local")) {
+        getLocalResources(event);
+    } else {
+        messenger_send(event.sender.id, response.respond(event.sender.id, event.message.text));
+    }
+    //messenger_send(event.sender.id, response.respond(event.sender.id, event.message.text));
+}
+
+function getLocalResources(event) {
+    var options = {
+        url: 'http://data.unhcr.org/api/whos_doing_what_where/settlements.json?instance_id=liberia',
+        method: 'GET'
+    };
+    function callback(error, resp, body) {
+        if (!error && resp.statusCode == 200) {
+            var info = JSON.parse(body);
+            messenger_send(event.sender.id, response.respond(event.sender.id, "local " + body.substring(0,50)));
+            console.log(info);
+        }
+    }
+    request(options, callback);
 }
 function addMonkeyIfPossible(event) {
-	if(response.respond(event.sender.id, event.message.text).indexOf("🐵") != -1 && response.respond(event.sender.id, event.message.text).indexOf("😄") != -1){
-    //here in respond could toggle if send and image or not! cause respond called here
-  	messenger_send_pic(event.sender.id, "http://orig14.deviantart.net/49d9/f/2017/161/2/5/monkeyvictor_by_chibixi-dbcal2t.gif");
-  } else if(response.respond(event.sender.id, event.message.text).indexOf("🐵") != -1){
-  	messenger_send_pic(event.sender.id, "http://orig12.deviantart.net/08c8/f/2017/161/b/f/monkeyassets_by_chibixi-dbcagez.gif");
-  }
+    if(response.respond(event.sender.id, event.message.text).indexOf("🐵") != -1 && response.respond(event.sender.id, event.message.text).indexOf("😄") != -1){
+        //here in respond could toggle if send and image or not! cause respond called here
+        messenger_send_pic(event.sender.id, "http://orig14.deviantart.net/49d9/f/2017/161/2/5/monkeyvictor_by_chibixi-dbcal2t.gif");
+    } else if(response.respond(event.sender.id, event.message.text).indexOf("🐵") != -1){
+        messenger_send_pic(event.sender.id, "http://orig12.deviantart.net/08c8/f/2017/161/b/f/monkeyassets_by_chibixi-dbcagez.gif");
+    }
 }
+
 function messenger_send_pic(userId,imageURL) {
     var options = {
-    uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=' + access_token,
-    method: 'POST',
-    json: {
-      'recipient':{
-        'id': userId
-      },
-      'message':{
-		"attachment":{
-		  "type":"image",
-		  "payload":{
-			"url":imageURL
-		  }
-		}
-	  }
-	  }
-  };
-  request(options);
-  var options2 = {
-    uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=' + access_token,
-    method: 'POST',
-    json: {
-      'recipient':{
-        'id': userId
-      },
-      "sender_action":"typing_on"
-	  }
-  };
-  request(options2);
+        uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=' + access_token,
+        method: 'POST',
+        json: {
+            'recipient':{
+                'id': userId
+            },
+            'message':{
+                "attachment":{
+                    "type":"image",
+                    "payload":{
+                        "url":imageURL
+                    }
+                }
+            }
+        }
+    };
+    request(options);
+    var options2 = {
+        uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=' + access_token,
+        method: 'POST',
+        json: {
+            'recipient':{
+                'id': userId
+            },
+            "sender_action":"typing_on"
+        }
+    };
+    request(options2);
 }
 
 var messenger_send = function (userId, text) {
-	text_portions = [];
-	while (text.length > 0) {
-		var portion = text.substr(0, 637);
-		if (portion.length == 637) {
-			portion = portion + '...';
-		}
-		text_portions.push(portion);
-		text = text.substr(637);
-	}
+    text_portions = [];
+    while (text.length > 0) {
+        var portion = text.substr(0, 637);
+        if (portion.length == 637) {
+            portion = portion + '...';
+        }
+        text_portions.push(portion);
+        text = text.substr(637);
+    }
 
-	var send_portion = function (i) {
-		if (i >= text_portions.length) {
-			return;
-		}
-		var options = {
-	    uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=' + access_token,
-	    method: 'POST',
-	    json: {
-	      'recipient':{
-	        'id': userId
-	      },
-	      'message':{
-	        'text': text_portions[i]
-	      }
-	    }
-	  };
-		request(options).on('response', function () {
-			send_portion(i+1);
-		});
-	}
-	send_portion(0);
+    var send_portion = function (i) {
+        if (i >= text_portions.length) {
+            return;
+        }
+        var options = {
+            uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=' + access_token,
+            method: 'POST',
+            json: {
+                'recipient':{
+                    'id': userId
+                },
+                'message':{
+                    'text': text_portions[i]
+                }
+            }
+        };
+        request(options).on('response', function () {
+            send_portion(i+1);
+        });
+    }
+    send_portion(0);
+
+    // messenger_send(event.sender.id, response.respond(event.sender.id, event.message.text));
 }
 
 app.listen(port, function() {
-  console.log("running at port " + port);
+    console.log("running at port " + port);
 });
--- response.js
+++ response.js
@@ -1,12 +1,16 @@
 var answers = require('./answers');
 //var storage = require('./storage');
+var request = require('request');
 
+var checkLocalResources = function (text) {
+	return text.includes("local");
+}
 var checkFave = function (text) {
 	return text.includes("you") && text.includes("fav")
 };
 
 var checkGreetings = function (text) {
-	return text.includes("hi ") || text.includes("hello") || text.includes("hey")
+	return text.includes("hi") || text.includes("hello") || text.includes("hey")
 };
 
 var cleanText = function (text) {
@@ -16,11 +20,15 @@ var cleanText = function (text) {
 
 var respond = function (userId, text) {
 	text = cleanText(text);
+	if (checkLocalResources(text)) {
+		return text.substring(5, text.length);
+	}
 	if (checkFave(text)) {
 		return "🐵 Aww thanks!😄"
 	}
 	if (checkGreetings(text)) {
-		return "🐵 I'm Madeliene!\nGreat to meet you!\n\nWhat are you interested in?\n1. I want to learn about sex\n2. I want to get hygiene products\n3. I want to talk to a real person"
+		//return "🐵 I'm Madeleine!\nGreat to meet you!\n\nWhat are you interested in?\n1. I want to learn about sex\n2. I want to get hygiene products\n3. I want to talk to a real person"
+		return "🐵 I'm Madeleine!\nGreat to meet you!\n\nWhat sexual health topic are you interested in?"
 	}
 	if (text == 'answer me') {
 		return answers[15][1]; // random answer
